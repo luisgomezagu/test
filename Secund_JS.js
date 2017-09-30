4e581ef4bfa87728d9090753a258945a8764160a
Luis Gomez <luisgomezagu@gmail.com>
2017-09-30 11:41:55 -0500

    alerta mensajes con variables

---
+++ Secund_JS.js
@@ -0,0 +1,6 @@
+var numberOne = prompt("insert numberOne");
+var numberTwo = prompt("insert numberTwo");
+numberOne = parseInt(numberOne);
+numberTwo = parseInt(numberTwo);
+var total= (numberOne + numberTwo);
+alert(total);
