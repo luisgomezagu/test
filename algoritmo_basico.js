911ef4c5229223bbaf5efc8565e72fb5d0c82e2a
Luis Gomez <luisgomezagu@gmail.com>
2017-09-30 12:06:44 -0500

    primer algoritmo

---
+++ algoritmo_basico.js
@@ -0,0 +1,12 @@
+var numberOne = 5;
+var numberTwo = 10;
+var bigger;
+
+    /*if (numberOne > numberTwo ){
+    bigger =numberOne;
+    }else {
+    bigger = numberTwo;
+    
+    }*/
+
+alert("jh");
